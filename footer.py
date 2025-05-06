3155c5edb683a93b178ac3ef1610b18a0ce47f51
nayoung <kny8493@gmail.com>
2025-05-06 19:04:22 +0900

    footer 오타 수정

--- footer.py
+++ footer.py
@@ -17,7 +17,7 @@ def footer(author="김나영"):
     st.markdown("<br><hr>", unsafe_allow_html=True)
     st.markdown(f"""
     <p style='text-align: center; font-size: 14px; color: gray;'>
-        Made by <b>{author}</b> | ⓒ 2025 토평고등학교 교육과정 박람회회<br>
+        Made by <b>{author}</b> | ⓒ 2025 토평고등학교 교육과정 박람회<br>
         Last Modified: {last_modified_str}<br>
         Contact: 2학년부 김나영(담당교과: 프로그래밍, 인공지능과 미래사회)
     </p>
