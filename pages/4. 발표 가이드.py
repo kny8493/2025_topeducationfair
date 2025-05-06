83a05b25a36ca9af407fc09233f4502224f97a38
nayoung <kny8493@gmail.com>
2025-05-06 19:25:43 +0900

    발표 가이드 추가

---
+++ pages/4. 발표 가이드.py
@@ -0,0 +1,13 @@
+
+import streamlit as st
+from footer import footer
+st.title("🎤 교과 서포터즈 발표 가이드")
+
+st.markdown("### 1️⃣ 왜 이 과목을 선택했나요?")
+st.markdown("### 2️⃣ 수업에서 가장 기억에 남는 활동은?")
+st.markdown("### 3️⃣ 선생님 수업 분위기와 장점은?")
+st.markdown("### 4️⃣ 이 과목을 선택하면 어떤 점이 좋아요?")
+st.markdown("### 5️⃣ 후배에게 한 문장으로 소개한다면?")
+st.markdown("### 6️⃣ 후배에게 해주고 싶은 말")
+
+footer()
